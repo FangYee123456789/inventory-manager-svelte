6f369c6d935378bb43de91fbd96bab17be15ef0b
shar <faethychan@gmail.com>
2026-04-07 18:05:13 +0800

    Create product log display

---
+++ frontend/src/features/product-log/product-log.tsx
@@ -0,0 +1,15 @@
+function ProductLog() {
+    return (
+        <section className="cell">
+            <article>
+                <h1>John Smith</h1>
+                <p>
+                    Added 5 quantity to Paint Roller (26) on 7 April 2026,
+                    5:39pm
+                </p>
+            </article>
+        </section>
+    );
+}
+
+export default ProductLog;
