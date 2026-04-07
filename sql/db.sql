d0f082258e600f2ad8a6d505195d370efca99846
shar <faethychan@gmail.com>
2026-04-07 18:02:39 +0800

    Complete db create tables

--- sql/db.sql
+++ sql/db.sql
@@ -1,41 +1,61 @@
-DROP TABLE IF EXISTS product_categories, products;
+DROP TABLE IF EXISTS 
+    users, 
+    product_categories, 
+    suppliers, 
+    products, 
+    delivery_orders, 
+    transactions;
 
--- CREATE TYPE user_roles AS ENUM ('QS, Procurement, Project')
+DROP TYPE IF EXISTS user_roles;
 
--- CREATE TABLE users (
---     id SERIAL NOT NULL PRIMARY KEY,
---     first_name TEXT NOT NULL,
---     last_name TEXT NOT NULL,
---     username VARCHAR(10) NOT NULL,
---     password TEXT NOT NULL,
---     role user_roles NOT NULL
--- )
+CREATE TYPE user_roles AS ENUM ('QS', 'Procurement', 'Project');
+
+CREATE TABLE users (
+    id SERIAL NOT NULL PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    username VARCHAR(10) NOT NULL,
+    password TEXT NOT NULL,
+    role user_roles NOT NULL
+);
 
 CREATE TABLE product_categories(
     id SERIAL NOT NULL PRIMARY KEY,
     name TEXT NOT NULL
 );
 
--- CREATE TABLE suppliers(
---     id SERIAL NOT NULL PRIMARY KEY,
---     name TEXT NOT NULL
--- )
+CREATE TABLE suppliers(
+    id SERIAL NOT NULL PRIMARY KEY,
+    name TEXT NOT NULL
+);
 
 CREATE TABLE products (
     id SERIAL NOT NULL PRIMARY KEY,
     name TEXT NOT NULL,
     cost NUMERIC(10, 2) NOT NULL,
-    photo_paths TEXT[] NOT NULL DEFAULT '{"./default_missing.jng"}',
+    photo_paths TEXT[] NOT NULL DEFAULT '{"./default_missing.jpg"}',
     url TEXT,
-    category_id INTEGER REFERENCES product_categories(ID),
-    -- supplier_id INTEGER NOT NULL REFERENCES Supplier(ID)
-    initial_quantity INTEGER NOT NULL DEFAULT 0
+    category_id INTEGER REFERENCES product_categories(id),
+    supplier_id INTEGER NOT NULL REFERENCES suppliers(id),
+    initial_quantity INTEGER NOT NULL DEFAULT 0,
+    current_quantity INTEGER NOT NULL DEFAULT NULL
 );
 
--- CREATE TABLE ProductLog(
---     id SERIAL NOT NULL PRIMARY KEY,
---     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
---     creator_id SERIAL NOT NULL REFERENCES
---     product_id SERIAL NOT NULL,
---     quantity_changed SMALLINT NOT NULL
--- )
+CREATE TABLE delivery_orders(
+    id TEXT NOT NULL PRIMARY KEY,
+    date DATE NOT NULL
+);
+
+CREATE TABLE transactions(
+    id SERIAL NOT NULL PRIMARY KEY,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    logger_id INT NOT NULL REFERENCES users(id),
+    product_id INT NOT NULL REFERENCES products(id),
+    quantity_changed INT NOT NULL,
+    delivery_id TEXT REFERENCES delivery_orders(id),
+    CONSTRAINT valid_transaction CHECK (
+      (quantity_changed > 0 AND delivery_id IS NOT NULL) 
+      OR
+      (quantity_changed < 0 AND delivery_id IS NULL)
+      )
+);
