b789da472faf8cf1ecdbb3af04f40e53f1e6e13c
shar <faethychan@gmail.com>
2026-05-14 17:26:46 +0800

    feat: create sign in form

--- src/routes/user/sign-in/+page.svelte
+++ src/routes/user/sign-in/+page.svelte
@@ -1,1 +1,14 @@
-<h1>sign in page</h1>
+<form>
+	<fieldset class="fieldset w-xs rounded-box border border-base-300 bg-base-200 p-4">
+		<legend class="fieldset-legend">Login</legend>
+		<label class="input">
+			<span class="label">Email:</span>
+			<input type="text" placeholder="example@domain.com" />
+		</label>
+		<label class="input">
+			<span class="label">Password:</span>
+			<input type="text" placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;" />
+		</label>
+		<button type="submit" class="btn">Sign in</button>
+	</fieldset>
+</form>
