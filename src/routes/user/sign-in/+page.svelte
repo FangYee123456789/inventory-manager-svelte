08fe0e3a92cb7688d9cbeb43b14f8b5890825d5d
shar <faethychan@gmail.com>
2026-05-15 11:54:38 +0800

    refactor: implement Form.svelte into sign in form

--- src/routes/user/sign-in/+page.svelte
+++ src/routes/user/sign-in/+page.svelte
@@ -1,50 +1,21 @@
 <script lang="ts">
+	import Form from '$lib/components/Form.svelte';
 	import Input from '$lib/components/Input.svelte';
 	import { signIn } from '$lib/remote/auth.remote';
 	const { email, password } = signIn.fields;
 
-	let isLoading: boolean = $state(false);
 	let isFilling: boolean = $state(true);
-
-	const onclick = () => {
-		isFilling = true;
-	};
+	const oninput = () => (isFilling = true);
 </script>
 
-<form
-	{...signIn.enhance(async ({ form, submit }) => {
-		isLoading = true;
-		if (await submit()) {
-			isFilling = false;
-			if (signIn.result?.success) form.reset();
-		}
-
-		isLoading = false;
-	})}
->
-	<fieldset
-		class="fieldset w-xs rounded-box border border-base-300 bg-base-200 p-4"
-		disabled={isLoading}
-	>
-		<legend class="fieldset-legend text-xl">Login</legend>
-		<Input label="Email" type="email" field={email} {onclick} placeholder="John" />
-		<Input
-			label="Password"
-			type="password"
-			field={password}
-			{onclick}
-			placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;"
-		/>
-		<button type="submit" class="btn">Sign in</button>
-		{#if isLoading}
-			<p>Loading...</p>
-		{/if}
-		{#if signIn.result && !isFilling}
-			{#if !signIn.result.success}
-				<p class="issue">Invalid credentials</p>
-			{:else}
-				<p class="text-green-600">Signed in</p>
-			{/if}
-		{/if}
-	</fieldset>
-</form>
+<Form remoteForm={signIn} errorMsg="Invalid credentials" successMsg="Signed in" {isFilling}>
+	<Input label="Email" type="email" field={email} {oninput} placeholder="example@domain.com" />
+	<Input
+		label="Password"
+		type="password"
+		field={password}
+		{oninput}
+		placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;"
+	/>
+	<button type="submit" class="btn">Sign in</button>
+</Form>
