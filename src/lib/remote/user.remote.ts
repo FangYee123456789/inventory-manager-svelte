f0850a425de0718de0d1cbdd9cbb82e03adbc5f5
shar <faethychan@gmail.com>
2026-05-15 10:16:23 +0800

    feat: add name to create user remote form

--- src/lib/remote/user.remote.ts
+++ src/lib/remote/user.remote.ts
@@ -7,23 +7,26 @@ import z from 'zod';
 
 export const createUser = form(
 	z
-		.object({ email, password, role: zString })
+		.object({ email, name: zString, password, role: zString })
 		.refine(({ role }) => role === 'QS' || role === 'Procurement' || role === 'Project', {
 			error: 'Role does not match any existing roles.',
 			path: ['role']
 		}),
-	async ({ email, password, role }, issue) => {
+	async ({ email, name, password, role }, issue) => {
 		try {
+			name = name[0].toUpperCase() + name.slice(1);
 			const passwordHash = await hashPassword(password);
 			const result =
-				await sql`INSERT INTO users (email, password_hash, role) VALUES(${email}, ${passwordHash}, ${role})`;
+				await sql`INSERT INTO users (email, name, password_hash, role) VALUES(${email}, ${name}, ${passwordHash}, ${role})`;
 			if (result.count !== 1) throw new Error('Failed to create user');
 		} catch (e) {
 			handleQueryErrors(e, (psqlError) => {
 				if (psqlError.code === '23505') {
 					switch (psqlError.constraint_name) {
 						case 'users_email_key':
-							invalid(issue.email('Email is already in use.'));
+							throw invalid(issue.email('Email is already in use.'));
+						case 'users_name_key':
+							throw invalid(issue.name('Name is already being used. Please add an initial.'));
 					}
 				}
 			});
