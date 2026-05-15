546373869c1ca9ca4bc709dd446f9dca661d6556
shar <faethychan@gmail.com>
2026-05-15 10:17:07 +0800

    feat: finish create user form

--- src/routes/user/add/+page.svelte
+++ src/routes/user/add/+page.svelte
@@ -1,27 +1,59 @@
 <script lang="ts">
-	let role = $state();
+	import { createUser } from '$lib/remote/user.remote';
+	const { email, password, role, name } = createUser.fields;
+
+	let isLoading: boolean = $state(false);
+	let disabled = $derived(isLoading);
 </script>
 
-<form>
-	<fieldset class="fieldset w-xs rounded-box border border-base-300 bg-base-200 p-4">
-		<legend class="fieldset-legend">Add new user</legend>
+<form
+	{...createUser.enhance(async ({ form, submit }) => {
+		isLoading = true;
+		if (await submit()) {
+			form.reset();
+		}
+		isLoading = false;
+	})}
+>
+	<fieldset {disabled} class="fieldset w-xs rounded-box border border-base-300 bg-base-200 p-4">
+		<legend class="fieldset-legend text-xl">Create new user</legend>
+		<input {...password.as('hidden', String(Math.floor(10000000 + Math.random() * 90000000)))} />
+		{#each password.issues() as issue, index (index)}
+			<p class="issue">{issue.message}</p>
+		{/each}
 		<label class="input mb-1">
 			<span class="label">Email:</span>
-			<input type="text" placeholder="example@domain.com" />
+			<input placeholder="example@domain.com" {...email.as('email')} />
 		</label>
+		{#each email.issues() as issue, index (index)}
+			<p class="issue">{issue.message}</p>
+		{/each}
+		<label class="input mb-1">
+			<span class="label">Name:</span>
+			<input placeholder="John" {...name.as('text')} />
+		</label>
+		{#each name.issues() as issue, index (index)}
+			<p class="issue">{issue.message}</p>
+		{/each}
 		<label>
-			<input type="radio" name="qs" class="radio" bind:group={role} value="QS" />
+			<input class="radio" {...role.as('radio', 'QS')} required />
 			QS
 		</label>
 		<label>
-			<input type="radio" name="procurement" class="radio" bind:group={role} value="Procurement" />
+			<input class="radio" {...role.as('radio', 'Procurement')} />
 			Procurement
 		</label>
 		<label>
-			<input type="radio" name="project" class="radio" bind:group={role} value="Project" />
+			<input class="radio" {...role.as('radio', 'Project')} />
 			Project
 		</label>
-
+		{#each role.issues() as issue, index (index)}
+			<p class="issue">{issue.message}</p>
+		{/each}
 		<button type="submit" class="btn mt-1">Create</button>
 	</fieldset>
 </form>
+
+{#if isLoading}
+	<p>Loading...</p>
+{/if}
