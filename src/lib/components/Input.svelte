ea8564a12745179722612f122a2aff40ffedc4c7
shar <faethychan@gmail.com>
2026-05-15 11:48:59 +0800

    feat: replace onclick with oninput in input.svelte

--- src/lib/components/Input.svelte
+++ src/lib/components/Input.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
-	const { label, type, field, onclick, placeholder = '' } = $props();
+	const {
+		label,
+		type,
+		field,
+		oninput = () => {
+			console.log('NO ONINPUT');
+		},
+		placeholder = ''
+	} = $props();
 </script>
 
 <label class="input">
 	<span class="label">{label}:</span>
-	<input {...field.as(type)} {placeholder} {onclick} />
+	<input {...field.as(type)} {placeholder} {oninput} />
 </label>
 {#each field.issues() as issue, i (i)}
 	<p class="issue">{issue.message}</p>
