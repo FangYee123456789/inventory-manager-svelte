1fcf56aebf761bc456fd2af8d5e0f34c4acebb22
shar <faethychan@gmail.com>
2026-05-18 13:37:06 +0800

    feat: throw error is items is undefined

--- src/routes/item/+page.server.ts
+++ src/routes/item/+page.server.ts
@@ -3,6 +3,7 @@ import type { Item } from '$lib/types/databaseTypes';
 
 export async function load() {
 	const items: Item[] | undefined = await getItemsFullInfo();
+	if (!items) throw new Error('getItemsFullInfo returned undefined');
 	return {
 		items
 	};
