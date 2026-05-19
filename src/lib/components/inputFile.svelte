0d88e86e1b37698e9aec983d257803eeff15e1eb
shar <faethychan@gmail.com>
2026-05-19 15:34:00 +0800

    style: add clear button to InputFile

--- src/lib/components/inputFile.svelte
+++ src/lib/components/inputFile.svelte
@@ -7,7 +7,10 @@
 <div class="mb-4">
 	<fieldset class="fieldset gap-0 p-0">
 		<legend class="fieldset-legend pt-0">{label}</legend>
-		<input {...field.as(type)} class="file-input" />
+		<div class="flex flex-row">
+			<input {...field.as(type)} class="file-input rounded-e-none" />
+			<button class="btn rounded-s-none btn-secondary">↻</button>
+		</div>
 		<span class="label">{subtitle}</span>
 	</fieldset>
 	<InputIssues {field} />
