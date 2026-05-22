f6812d14c503e30d989dea69076f2d18accdb62a
shar <faethychan@gmail.com>
2026-05-22 09:37:11 +0800

    feat: create input for editing master and name

--- src/routes/item/[slug]/+page.svelte
+++ src/routes/item/[slug]/+page.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
 	import { resolve } from '$app/paths';
+	import Form from '$lib/components/base/form.svelte';
+	import Input from '$lib/components/base/input.svelte';
+	import { editMaster, editName } from '$lib/remote/item.remote';
 	import type { Item } from '$lib/types/databaseTypes.js';
 
 	const { data } = $props();
-	let { master, name, category, categoryID, supplier, supplierID, quantity }: Item = $derived(
+	let { id, master, name, category, categoryID, supplier, supplierID, quantity }: Item = $derived(
 		data.item
 	);
 </script>
@@ -20,14 +23,19 @@
 	404 not found
 {:else}
 	<div class="max-w-75">
-		<h1>{name}</h1>
-		<h2>{master}</h2>
-		<select name="" id="">
-			<option value={categoryID}>{category}</option>
-		</select>
-		<select name="" id="">
-			<option value={supplierID}>{supplier}</option>
-		</select>
-		<p>{quantity}</p>
+		{@render editForm(editMaster, editMaster.fields.master, master, 'Master')}
+		{@render editForm(editName, editName.fields.name, name, 'Name')}
 	</div>
 {/if}
+
+<!-- eslint-disable-next-line @typescript-eslint/no-explicit-any -->
+{#snippet editForm(remoteForm: any, field: unknown, value: string, label: string)}
+	<Form
+		{remoteForm}
+		errorMsg="Failed to update master number."
+		successMsg="Master number has been updated."
+	>
+		<input {...remoteForm.fields.id.as('hidden', id)} />
+		<Input {field} {value} type="text" placeholder={value} {label} rightButton="Edit" />
+	</Form>
+{/snippet}
