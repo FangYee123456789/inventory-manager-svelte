0b6eedfbeb030c912eacf0a913869dd3be7703c0
shar <faethychan@gmail.com>
2026-05-25 10:06:42 +0800

    feat: add getItemName remote query

--- src/lib/remote/item.remote.ts
+++ src/lib/remote/item.remote.ts
@@ -251,3 +251,13 @@ export const editSupplier = form(
 		}
 	}
 );
+
+export const getItemName = query(zString, async (master) => {
+	try {
+		const result = await sql<Item[]>`SELECT name FROM items WHERE master_number = ${master}`;
+		if (result.count !== 1) return undefined;
+		return result[0];
+	} catch (e) {
+		handleQueryErrors(e);
+	}
+});
