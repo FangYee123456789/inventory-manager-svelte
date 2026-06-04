5d1d82f503750171f948027603209bcedcf71574
shar <faethychan@gmail.com>
2026-06-04 14:28:44 +0800

    feat: implement removing items from TransactionItemTable

--- src/lib/components/transactionItemTable.svelte
+++ src/lib/components/transactionItemTable.svelte
@@ -3,6 +3,7 @@
 	import { createIncomingTransaction } from '$lib/remote/transaction.remote.js';
 	import type { Item } from '$lib/types/databaseTypes.js';
 	import { truncateString } from '$lib/utils/stringTransform.js';
+	import { tick } from 'svelte';
 
 	const { ids, quantities } = createIncomingTransaction.fields;
 
@@ -16,6 +17,7 @@
 <table class="table table-zebra">
 	<thead>
 		<tr>
+			<th></th>
 			<th class="w-15">Master</th>
 			<th>Name</th>
 			<th class="w-10">Qty</th>
@@ -24,11 +26,21 @@
 	<tbody>
 		{#each items as { id, master, name, quantity }, i (i)}
 			<tr>
+				<th
+					><button
+						aria-label="delete"
+						type="button"
+						onclick={async () => {
+							items.splice(i, 1);
+							await tick();
+						}}><span class="icon-[tabler--trash]"></span></button
+					></th
+				>
 				<th class="w-15">{master}</th>
 				<th>{truncateString(name, 20)}</th>
 				<th
 					><input
-						class="w-10 border ps-1"
+						class="w-25 border ps-1"
 						type="number"
 						id={`input${i}`}
 						name={`input${i}`}
