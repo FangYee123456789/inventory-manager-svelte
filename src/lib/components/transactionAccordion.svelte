01918b24fcb8d6cdcc89ec9f70b9500d75ed6d54
shar <faethychan@gmail.com>
2026-06-18 09:28:47 +0800

    refactor: extract individual transaction accordion to implement deletion

---
+++ src/lib/components/transactionAccordion.svelte
@@ -0,0 +1,108 @@
+<script lang="ts">
+	import { deleteTransaction } from '$lib/remote/transaction.remote';
+	import type { CompleteTransaction } from '$lib/types/databaseTypes';
+	import { formatRelativeCustom } from '$lib/utils/dateTransform';
+	import { format } from 'date-fns';
+	import { toast } from 'svelte-sonner';
+
+	type Props = {
+		transaction: CompleteTransaction;
+	};
+
+	const { transaction }: Props = $props();
+	const {
+		id,
+		createdAt,
+		deliveryDate,
+		supplier,
+		deliveryID,
+		expendDate,
+		expender,
+		remarks,
+		items
+	} = $derived(transaction);
+	const isIncoming = $derived(!expendDate);
+	const modalID = $derived.by(() => {
+		return `confirm-modal${id}-${format(createdAt, 'yyyy-MM-dd')}`;
+	});
+	const uniqueForm = $derived(deleteTransaction.for(modalID));
+
+	function openDeleteConfirmation() {
+		const modal = document.querySelector(`#${modalID}`);
+		(modal as HTMLDialogElement).showModal();
+	}
+
+	function closeDeleteConfirmation() {
+		const modal = document.querySelector(`#${modalID}`);
+		(modal as HTMLDialogElement).close();
+	}
+</script>
+
+<details class="collapse border border-base-300 bg-base-100" name="my-accordion-det-1">
+	<summary class="collapse-title font-semibold">
+		<div class="flex items-center justify-between gap-10">
+			{#if isIncoming}
+				<span>
+					{supplier}
+					{format(deliveryDate!, 'yyyy/MM/dd')}
+					{deliveryID}
+				</span>
+			{:else}
+				<span>
+					{expender}
+					{format(expendDate!, 'yyyy/MM/dd')}
+					{remarks}
+				</span>
+			{/if}
+			<span>
+				{formatRelativeCustom(createdAt)}
+				<button
+					class="btn size-12.5 btn-soft btn-error"
+					aria-label="delete"
+					onclick={openDeleteConfirmation}
+				>
+					<span class="icon-[tabler--trash]"></span>
+				</button>
+			</span>
+		</div>
+	</summary>
+	<div class="collapse-content text-sm">
+		<ul>
+			{#each items as item, i (i)}
+				<li class="ms-6 list-disc">
+					#{item.master}
+					{item.name}
+					{isIncoming ? '+' : '-'}{item.quantity}
+				</li>
+			{/each}
+		</ul>
+	</div>
+</details>
+
+<dialog id={modalID} class="modal">
+	<div class="modal-box">
+		<h2 class="text-lg">Are you sure you want to delete this transaction?</h2>
+		<div class="modal-action">
+			<form method="dialog">
+				<button class="btn btn-soft btn-secondary">Cancel</button>
+			</form>
+			<form
+				{...uniqueForm.enhance(async ({ submit }) => {
+					if (await submit()) {
+						closeDeleteConfirmation();
+						const { success, message } = uniqueForm.result!;
+						if (success) {
+							toast.success('Transaction has been deleted.');
+						} else {
+							toast.error(message || 'Failed to delete transaction.');
+						}
+					}
+				})}
+			>
+				<input {...uniqueForm.fields.id.as('hidden', id)} />
+				<input {...uniqueForm.fields.isIncoming.as('checkbox', isIncoming)} class="invisible" />
+				<button class="btn btn-primary">Confirm</button>
+			</form>
+		</div>
+	</div>
+</dialog>
--- src/lib/components/transactionAccordionGroup.svelte
+++ src/lib/components/transactionAccordionGroup.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import type { CompleteTransaction } from '$lib/types/databaseTypes';
-	import { formatRelativeCustom } from '$lib/utils/dateTransform';
+	import TransactionAccordion from './transactionAccordion.svelte';
 
 	type Props = {
 		transactions: CompleteTransaction[];
@@ -9,31 +9,6 @@
 	const { transactions }: Props = $props();
 </script>
 
-{#each transactions as { id, createdAt, supplier, deliveryID, expender, remarks, items } (id)}
-	<details class="collapse border border-base-300 bg-base-100" name="my-accordion-det-1">
-		<summary class="collapse-title font-semibold">
-			<div class="flex items-center justify-between gap-10">
-				<span
-					>{supplier || expender}{deliveryID ? ` ${deliveryID}` : remarks ? `, ${remarks}` : ''}
-				</span>
-				<span>
-					{formatRelativeCustom(createdAt)}
-					<button class="btn ms-2 btn-soft btn-accent" aria-label="focus">
-						<span class="icon-[ion--navigate]"></span>
-					</button>
-				</span>
-			</div>
-		</summary>
-		<div class="collapse-content text-sm">
-			<ul>
-				{#each items as item, i (i)}
-					<li class="ms-6 list-disc">
-						#{item.master}
-						{item.name}
-						{deliveryID ? '+' : '-'}{item.quantity}
-					</li>
-				{/each}
-			</ul>
-		</div>
-	</details>
+{#each transactions as transaction, i (i)}
+	<TransactionAccordion {transaction} />
 {/each}
