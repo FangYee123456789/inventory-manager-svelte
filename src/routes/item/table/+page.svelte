babc42aba98060c2906a82586f7d2e5afbb8eccd
shar <faethychan@gmail.com>
2026-06-24 10:07:04 +0800

    feat: convert table contents to array

--- src/routes/item/table/+page.svelte
+++ src/routes/item/table/+page.svelte
@@ -1,12 +1,33 @@
 <script lang="ts">
+	import { toast } from 'svelte-sonner';
+
 	const { data } = $props();
 	const dates = $derived.by(() => {
 		const itemIDs = Object.keys(data.timeline);
 		const firstID = itemIDs[0];
 		return data.timeline[firstID].map((week) => week);
 	});
+
+	function onclick() {
+		let csvData: string[] = [];
+
+		let rows = document.getElementsByTagName('tr');
+		for (let i = 0; i < rows.length; i++) {
+			let cols = rows[i].querySelectorAll('td,th');
+
+			let csvrow = [];
+			for (let j = 0; j < cols.length; j++) {
+				csvrow.push(cols[j].innerHTML);
+			}
+
+			csvData.push(csvrow.join(','));
+		}
+		console.log(csvData.join('\n'));
+		console.log(csvData);
+	}
 </script>
 
+<button {onclick}>Copy</button>
 <table class="table">
 	<thead>
 		<tr>
