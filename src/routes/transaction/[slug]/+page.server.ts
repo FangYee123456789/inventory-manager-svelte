1cb517cbe5b8c2e2d37ad863c5bb264daace44bd
shar <faethychan@gmail.com>
2026-07-10 10:50:01 +0800

    feat: fix permissions for editing transactions

--- src/routes/transaction/[slug]/+page.server.ts
+++ src/routes/transaction/[slug]/+page.server.ts
@@ -3,18 +3,32 @@ import type { CompleteTransaction } from '$lib/types/databaseTypes';
 import { error } from '@sveltejs/kit';
 
 export async function load({ locals, params, url }) {
-	if (!locals.user || !(locals.user.role === 'Procurement' || locals.user.role === 'Admin'))
+	if (
+		!locals.user ||
+		!(
+			locals.user.role === 'Procurement' ||
+			locals.user.role === 'Admin' ||
+			locals.user.role === 'Project'
+		)
+	) {
 		error(403, 'Forbidden');
+	}
 
 	const transactionType = url.searchParams.get('type');
 	let transaction: CompleteTransaction;
 
-	if (transactionType === 'incoming' && locals.user.role === 'Procurement') {
+	if (
+		transactionType === 'incoming' &&
+		(locals.user.role === 'Procurement' || locals.user.role === 'Admin')
+	) {
 		transaction = await getIncomingTransaction(params.slug);
-	} else if (transactionType === 'outgoing' && locals.user.role === 'Project') {
+	} else if (
+		transactionType === 'outgoing' &&
+		(locals.user.role === 'Project' || locals.user.role === 'Admin')
+	) {
 		transaction = await getOutgoingTransaction(params.slug);
 	} else {
-		error(400, 'Bad request');
+		error(403, 'Forbidden or bad request');
 	}
 
 	return { transaction, isIncoming: transactionType === 'incoming' };
